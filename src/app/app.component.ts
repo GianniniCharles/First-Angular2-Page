2415fd1616010c7753aee9a6012d0129f39ec6f9
Giannini Charles <12gcharles@gmail.com>
2018-07-23 15:51:50 -0400

    Created first lesson in Angular2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title:string = 'blah'
+  arrayOfAnimals: Array<any> = [
+    {
+      id: 1,
+      category: 'mammal',
+      name: 'Dog'
+    }, {
+      id: 2,
+      category: 'oviparous',
+      name: 'Duck'
+    }, {
+      id: 3,
+      category: 'mammal',
+      name: 'Elephant'
+    }, {
+      id: 4,
+      category: 'reptile',
+      name: 'Snake'
+    }
+  ];
 }
